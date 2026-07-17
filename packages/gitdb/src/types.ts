1e0fe26c916a930e8d03f74377bf1fe51a294a6e
Carlos Lopez <carlos@kettu.studio>
2026-07-17 17:09:06 +0200

    remove package from repo

--- packages/gitdb/src/types.ts
+++
@@ -1,9 +0,0 @@
-export type GitDbOptions = {
-  repositoryUrl: string;
-  autoCommitIntervalMs?: number;
-  immediateCommitDelayMs?: number;
-  gitUserName?: string;
-  gitUserEmail?: string;
-  logger?: import('./infrastructure/logger.ts').GitDbLoggerLike;
-};
-
--- packages/gitdb/test-schema.ts
+++
@@ -1,6 +0,0 @@
-import { entity } from './dist/index.js';
-
-export const users = entity('users', {
-  id: 'int',
-  name: 'string',
-});
